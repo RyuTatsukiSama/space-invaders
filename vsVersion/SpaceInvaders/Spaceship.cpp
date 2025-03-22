b4158653a6a01dba2acb075bee51dd6e20a830cf
RyuTatsukiSama <dorianribault@gmail.com>
2025-03-22 01:37:04 +0100

    Reorganized the project + tried to import the project into VS Code + unzip dependencies

---
+++ vsVersion/SpaceInvaders/Spaceship.cpp
@@ -0,0 +1,39 @@
+#include "Spaceship.h"
+
+Spaceship::Spaceship(EnergyBoltPool * pool) : bolt_pool_(pool)
+{
+	transform_.setPosition(1000, 500);
+	w_ = h_ = 40;
+
+	texture_.loadFromFile("images/spaceship.png");
+	texture_.setSmooth(true);
+	sprite_.setTexture(texture_);
+	sprite_.setTextureRect(sf::IntRect(40, 0, w_, h_));
+	sprite_.setOrigin(w_ / 2, h_ / 2);
+}
+
+Spaceship::~Spaceship()
+{
+}
+
+void Spaceship::openFire()
+{
+	PlasmaBolt* bolt = bolt_pool_->createBolt(Plasma::RED);
+	if (bolt)
+		bolt->moveTo(transform_.getPosition().x-w_/4, transform_.getPosition().y - h_/2);
+}
+
+void Spaceship::moveTo(float xpos, float ypos)
+{
+	transform_.setPosition(xpos, ypos);
+}
+
+void Spaceship::move(float dx, float dy)
+{
+	transform_.move(dx, dy);
+}
+
+void Spaceship::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(sprite_, transform_.getTransform());
+}
